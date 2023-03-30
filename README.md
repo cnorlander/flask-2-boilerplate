04c5c141b4eafa5782482b36b76347b593d6e499
Corey Norlander <corey@flaresoftworks.com>
2023-03-30 16:26:11 -0700

    README Update & Bug Fix

--- README.md
+++ README.md
@@ -10,7 +10,8 @@ This is a basic Flask web application created to be a jumping off point for futu
 - [X] A menu template
 - [ ] Some basic page templates.
 - [ ] A user management page.
-- [ ] An easy to use and flexible role and action system for managing permissions
+- [ ] A role management page.
+- [X] An easy to use and flexible role and action system for managing permissions
 - [X] Docker and Docker-Compose files for easy deployment
 - [X] Gunicorn WSGI HTTP Server
 - [X] NGINX for reverse proxy and serving static files
--- boilerplate/app.py
+++ boilerplate/app.py
@@ -24,16 +24,16 @@ with app.app_context():
     import boilerplate.errors
     import boilerplate.utils.filters
     import boilerplate.modules.role as role
-    import boilerplate.modules.user
-    import boilerplate.modules.login
+    import boilerplate.modules.user as user
+    import boilerplate.modules.login as login
 
     # Create any db models
     db.create_all()
     db.session.commit()
 
     # Run the database initialization seeding tasks if needed
-    #role.role_model.seed_roles_if_required()
+    role.role_model.seed_roles_if_required()
     role.role_model.update_system_roles()
     #role.role_model.action_clean_up()
-    #user.user_model.seed_user_if_required()
+    user.user_model.seed_user_if_required()
 
